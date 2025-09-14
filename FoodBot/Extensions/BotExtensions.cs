c046efcf99d571355675d3359a9d21e13f553910
artemmail <artemmail@gmail.com>
2025-09-14 22:36:05 +0300

    chore: handle hub auth query tokens more safely

--- FoodBot/Extensions/BotExtensions.cs
+++ FoodBot/Extensions/BotExtensions.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using System.Threading.Tasks;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Telegram.Bot;
@@ -167,6 +168,21 @@ public static class BotExtensions
                     ValidateAudience = true,
                     ValidateLifetime = true,
                     ClockSkew = TimeSpan.FromMinutes(2)
+                };
+
+                opts.Events ??= new JwtBearerEvents();
+                opts.Events.OnMessageReceived = context =>
+                {
+                    if (string.IsNullOrEmpty(context.Token))
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
+                        {
+                            context.Token = accessToken;
+                        }
+                    }
+                    return Task.CompletedTask;
                 };
             });
 
