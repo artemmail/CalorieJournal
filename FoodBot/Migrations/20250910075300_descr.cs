daeac341b9d54fb75fbc29d164abac332197c858
ruticker1 <ruticker@gmail.com>
2025-09-15 06:40:39 +0300

    add migr

---
+++ FoodBot/Migrations/20250910075300_descr.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class descr : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "PendingMeals",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "GenerateImage",
+                table: "PendingMeals",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "PendingMeals");
+
+            migrationBuilder.DropColumn(
+                name: "GenerateImage",
+                table: "PendingMeals");
+        }
+    }
+}
---
+++ FoodBot/Migrations/20250910093439_descr1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class descr1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
